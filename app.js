3049ac130625ccc0dd91463dbd3d56eb8a1b948d
ismet kizgin <info@ismetkizgin.com.tr>
2022-01-02 01:33:43 +0300

    feat: added swagger

--- app.js
+++ app.js
@@ -2,14 +2,49 @@ require("dotenv/config");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const { version } = require("./package.json");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 
+const swaggerSpecs = swaggerJsdoc({
+  swaggerDefinition: {
+    openapi: "3.0.1",
+    info: {
+      title: "Swager Example",
+      version: version,
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+        apiKey: {
+          type: "apiKey",
+          name: "x-api-key",
+          in: "header",
+        },
+      },
+    },
+    security: [
+      {
+        apiKey: [],
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+app.use(require("./routes/auth").router);
 
 app.listen(PORT, () => {
   console.log("Ready on http://localhost:" + PORT);
---
+++ routes/auth.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = express.Router();
+
+/**
+ * @swagger
+ * tags:
+ *  name: MainData
+ *  description: This is for the main data
+ * /getData:
+ *  get:
+ *      tags: [MainData]
+ *      parameters:
+ *          - name: page_number
+ *            default: 1
+ *            in: query
+ *            schema:
+ *              type: integer
+ *          - name: page_length
+ *            default: 10
+ *            in: query
+ *            required: true
+ *            schema:
+ *              type: integer
+ *      responses:
+ *          default:
+ *              description: This is the default response for it
+ *      security:
+ *      - bearerAuth: []
+ */
+router.get("/getData", (req, res) => {
+  const { page_number, page_length } = req.query;
+
+  res.send(req.query);
+});
+
+/**
+ * @swagger
+ * tags:
+ *  name: MainData
+ *  description: This is for the main data
+ * /login:
+ *  post:
+ *      tags: [MainData]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          email:
+ *                              type: string
+ *                              default: coder
+ *                          password:
+ *                              type: string
+ *                              default: coder123
+ *      responses:
+ *          default:
+ *              description: This is the default response for it
+ */
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const out = {
+    token: "thisIsAJWTTokenKeepItSafe",
+  };
+  res.send(out);
+});
+
+module.exports = {
+  router,
+};
